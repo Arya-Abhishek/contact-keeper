bbc0253512ad502262a218bb42bbb5c3e13a7541
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-10-01 17:01:47 +0530

    added User model and User validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const { validationResult, check } = require("express-validator");
+
+const User = require("../models/User");
+
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add a name").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password of min length 6").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("passed");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to Contact Keeper API..." })
 );
@@ -13,7 +16,7 @@ app.get("/", (req, res) =>
 // DEFINE ROUTES
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/conmodeltacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
