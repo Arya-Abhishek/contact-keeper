d7ec90d9fa2a06fbd3ee9c59ed8e0a75ec44f082
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-10-01 20:59:44 +0530

    added Login User and Register User

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const { validationResult, check } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route       GET api/auth
 // @desc        Get logged in user
@@ -11,8 +18,56 @@ router.get("/", (req, res) => {
 // @route       GET api/auth
 // @desc        Auth user and get token
 // @access      Public
-router.post("/", (req, res) => {
-  res.send("User Logged in");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "User doesn't exists" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Password didn't match!" });
+      }
+
+      // If it got matched then just return the jwt token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
