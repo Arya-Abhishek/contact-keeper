fcc4f1734bc5b93b709dfee5a99e9fe9d3aed905
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-10-02 09:31:14 +0530

    fixed POST Auth user and get token

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-// @route       GET api/auth
+// @route       POST api/auth
 // @desc        Auth user and get token
 // @access      Public
 router.post(
